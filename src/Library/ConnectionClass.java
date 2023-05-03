2f47b6a26d3dc8bba477ffa96aaa40e2cf0c9ae7
Rahil Khan <rahilkhanmakki001@gmail.com>
2023-05-03 09:14:16 +0530

    initial commit

---
+++ src/Library/ConnectionClass.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Library;
+import java.sql.*;
+/**
+ *
+ * @author rahil
+ */
+public class ConnectionClass {
+    Connection con;
+    Statement stm;
+    ConnectionClass(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library","root","Rahil123");
+            stm = con.createStatement();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args){
+        new ConnectionClass();
+    }
+    
+}
